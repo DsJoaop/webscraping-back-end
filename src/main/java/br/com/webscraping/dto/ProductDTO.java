6567ed6e1ded9959b6586699701d0dd66c0679cd
Dsjoaop <joaopaulo.eu29@hotmail.com>
2024-08-12 23:06:56 -0300

    Init JUnit Testes

--- src/main/java/br/com/webscraping/dto/ProductDTO.java
+++ src/main/java/br/com/webscraping/dto/ProductDTO.java
@@ -1,23 +1,17 @@
+// src/main/java/br/com/webscraping/dto/ProductDTO.java
 package br.com.webscraping.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serial;
-import java.io.Serializable;
-
 @Data
 @NoArgsConstructor
-public class ProductDTO implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
-
-
+public class ProductDTO {
     private Long id;
     private String name;
     private String description;
-    private Double price;
+    private double price;
     private String imgUrl;
     private String url;
-
+    private String category;
 }
--- src/main/java/br/com/webscraping/entities/Product.java
+++ src/main/java/br/com/webscraping/entities/Product.java
@@ -18,7 +18,7 @@ public class Product implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
